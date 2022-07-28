19f3840a887a380fb52474a7df5d3ef6e49de13c
Valdir Silva <valdipiresba@gmail.com>
2022-07-27 23:48:43 -0300

    Acessando backend via axios

--- frontend/src/Card/Card.tsx
+++ frontend/src/Card/Card.tsx
@@ -1,10 +1,23 @@
 import styles from './card.module.css';
 import Item from './Item';
+import { api } from '../Services/api';
+import { useEffect, useState } from 'react';
+
 
 export function Card() {
+    const [restaurants, setRestaurants] = useState([]);
+
+    useEffect(() => {
+
+        async function getRestaurants() {
+            const response = await api.get('restaurants')
+
+            console.log(response);
+        }
+
+    }, []);
+
     return (
-
-
         <div className={styles.panel}>
             <div className={styles.panelTitle}>
                 <h3>Restaurantes</h3>
