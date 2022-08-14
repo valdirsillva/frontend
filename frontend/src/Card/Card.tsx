2ce5894277a7adde39f0585b9f64fec34115a53a
Valdir Silva <valdipiresba@gmail.com>
2022-08-14 16:59:53 -0300

    Integrando o frontend com backend api

--- frontend/src/Card/Card.tsx
+++ frontend/src/Card/Card.tsx
@@ -1,22 +1,7 @@
 import styles from './card.module.css';
 import Item from './Item';
-import { api } from '../Services/api';
-import { useEffect, useState } from 'react';
-
 
 export function Card() {
-    const [restaurants, setRestaurants] = useState([]);
-
-    useEffect(() => {
-
-        async function getRestaurants() {
-            const response = await api.get('restaurants')
-
-            console.log(response);
-        }
-
-    }, []);
-
     return (
         <div className={styles.panel}>
             <div className={styles.panelTitle}>
--- frontend/src/Card/Item.tsx
+++ frontend/src/Card/Item.tsx
@@ -1,31 +1,45 @@
 import { Link } from 'react-router-dom';
-
 import styles from './card.module.css';
-import images from '../images/restaurant1.png';
+
+import { api } from '../Services/api';
+import { useEffect, useState } from 'react';
+
+type Restaurant = {
+    id: string,
+    image: string,
+    name: string,
+    opening_hours: string,
+    payment_method: string,
+    price: string
+}
 
 export default function Item() {
+    const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+
+    useEffect(() => {
+        api.get<Restaurant[]>('restaurants').then(({ data }) => {
+            setRestaurants(data)
+        })
+
+    }, []);
+
     return (
         <>
-            <div className={styles.overlay}>
-                <div className={styles.itemImage}>
-                    <Link to="/details">
-                        <img src={images} alt="" />
-                        <span>Flor de juá</span>
-                    </Link>
-                </div>
-            </div>
+            {restaurants.map(restaurant => {
+                let img = `http://localhost:8888/images/`
 
-            <div className={styles.overlay}>
-                <div className={styles.itemImage}>
-                    <Link to="/details">
-                        <img src={images} alt="" />
-                        <span>Flor de Manjericão</span>
-                    </Link>
-                </div>
-            </div>
-
-
-
+                console.log(restaurant)
+                return (
+                    <div className={styles.overlay} key={restaurant.id}>
+                        <div className={styles.itemImage}>
+                            <Link to="/details">
+                                <img src={`${img}${restaurant.image}`} alt="" />
+                                <span>{restaurant.name}</span>
+                            </Link>
+                        </div>
+                    </div>
+                )
+            })}
         </>
     )
 }
